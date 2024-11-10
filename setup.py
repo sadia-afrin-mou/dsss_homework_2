b55439befa0d3157d16c9126fa7e22517f57e397
Sadia Afrin Mou <sadia.afrin.mou@fau.de>
2024-11-10 22:20:33 +0100

    pip installable feature

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dsss_homework_2",
+    version="0.1.0",
+    description="A simple interactive math quiz game",
+    author="Sadia Afrin Mou",
+    author_email="sadia.afrin.mou@fau.de",  # Your email
+    url="https://github.com/sadia-afrin-mou/dsss_homework_2",  # URL to the project's homepage
+    license="Apache License 2.0",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "dsss_homework_2=math_quiz.math_quiz:math_quiz",
+        ],
+    },
+)
